a2fbca035166d70adbf476d403c3e27a2b01863b
JacoHg <jacohenningpop@gmail.com>
2024-04-28 15:10:00 +0200

    lastest changes

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:dice/gradient_container.dart';
+
+void main() {
+  runApp(
+     const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(colors: [Colors.amber, Colors.purple],)
+      ),
+    ),
+  );
+}
+
+
+  
+ 
